d47effd8f07ba8cc0d5fcdaba5a29454e1ad292a
차지원 <chajiwon100785@gmail.com>
2020-11-05 17:15:38 +0900

    비기너 트랙 : constraint layout

---
+++ Happy Birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.happybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+fun main() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
