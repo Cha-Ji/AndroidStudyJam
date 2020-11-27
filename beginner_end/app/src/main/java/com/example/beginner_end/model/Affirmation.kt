ab52a7e90dc8ce5fb7a0ac1d628738b38aaa67c8
차지원 <chajiwon100785@gmail.com>
2020-11-28 03:47:08 +0900

    리사이클러 예제 포기!

---
+++ beginner_end/app/src/main/java/com/example/beginner_end/model/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.example.beginner_end.model
+
+data class Affirmation(private val sportsImage:String,
+private val sportsTitle:String,
+private val sportsInfo:String)
---
+++ beginner_end/app/src/main/java/com/example/beginner_end/model/DataSource.kt
@@ -0,0 +1,5 @@
+package com.example.beginner_end.model
+
+class DataSource{
+    fun loadSports()
+}
